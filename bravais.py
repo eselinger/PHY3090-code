cd889cb39471c44aa0024415ad892c8d14299e98
eselinger <elizabeth.selinger@uoit.net>
2015-01-15 12:55:25 -0500

    write out to file

--- bravais.py
+++ bravais.py
@@ -11,11 +11,10 @@ def main():
     a = 1
 
     if bl=='sc':
-        numat = numcells* #give total number of atoms from type of BL and # unit cells
         basis = np.array([0,0,0])
-        a1 = [a,0,0]
-        a2 = [0,a,0]
-        a3 = [0,0,a]
+        a1 = np.array([a,0,0])
+        a2 = np.array([0,a,0])
+        a3 = np.array([0,0,a])
 
     lattice0 = []
 
@@ -23,14 +22,14 @@ def main():
     for z in range(numcells): #place atoms in z
         for i in range(len(basis)):
             atom = basis[i] + a3*z
-             lattice0.append(atom)
-             lattice.append(atom)
+            lattice0.append(atom)
+            lattice.append(atom)
     print lattice0
 
     lattice1 = []
 
     for y in range(numcells):
-        for j in range(len(lattice0):
+        for j in range(len(lattice0)):
             atom = lattice0[j] + a2*y
             lattice1.append(atom)
             lattice.append(atom)
@@ -43,4 +42,11 @@ def main():
 
     output = open('bravais_lattice.xyz', 'w')
     output.write(str(len(lattice)) + '\n \n')
-    
+   
+    for a in range(len(lattice)):
+        for element in lattice[a]: output.write(X + '     ' + str(element) + '     ')
+        output.write('\n')
+
+    output.close()
+
+main()
