8e36be8e18bed10b899b2ba215d84a612973c6dd
eselinger <elizabeth.selinger@uoit.net>
2015-01-17 20:29:43 -0500

    tetragonal and orthorhombic structures

--- bravais.py
+++ bravais.py
@@ -10,26 +10,79 @@ def main():
     n3 = int(sys.argv[4])+1
 
     a = 1.
-    b = a*0.5
-    c = 0
 
     if bl=='sc':
+        b = a
+        c = a
         basis = np.array([[0.,0.,0.]])
         a1 = np.array([a, 0., 0.])
         a2 = np.array([0., a, 0.])
         a3 = np.array([0., 0., a])
 
     if bl=='bcc':
-        basis = np.array([[0., 0., 0.],[b, b, b]])
+        b = a
+        c = a
+        basis = np.array([[0., 0., 0.],[a*0.5, a*0.5, a*0.5]])
         a1 = np.array([a, 0., 0.])
         a2 = np.array([0., a, 0.])
         a3 = np.array([0., 0., a])
 
     if bl=='fcc':
-        basis = np.array([[0., 0., 0.],[b, b, 0.],[b, 0., b],[0., b, b]])
+        b = a
+        c = a
+        basis = np.array([[0., 0., 0.],[a*0.5, a*0.5, 0.],[a*0.5, 0., a*0.5],[0., a*0.5, a*0.5]])
         a1 = np.array([a, 0., 0.])
         a2 = np.array([0., a, 0.])
         a3 = np.array([0., 0., a])     
+
+    if bl=='st':
+        b = a
+        c = 2*a
+        basis = np.array([[0., 0., 0.]])
+        a1 = np.array([a, 0., 0.])
+        a2 = np.array([0., a, 0.])
+        a3 = np.array([0., 0., c])
+
+    if bl=='bct':
+        b = a
+        c = 3*a
+        basis = np.array([[0., 0., 0.],[a*0.5, a*0.5, c*0.5]])
+        a1 = np.array([a, 0., 0.])
+        a2 = np.array([0., a, 0.])
+        a3 = np.array([0., 0., c])
+
+    if bl=='so':
+        b = 2*a
+        c = 3*a
+        basis = np.array([[0.,0.,0.]])
+        a1 = np.array([a, 0., 0.])
+        a2 = np.array([0., b, 0.])
+        a3 = np.array([0., 0., c])
+
+    if bl=='bco':
+        b = 2*a
+        c = 3*a
+        basis = np.array([[0., 0., 0.],[a*0.5, b*0.5, c*0.5]])
+        a1 = np.array([a, 0., 0.])
+        a2 = np.array([0., b, 0.])
+        a3 = np.array([0., 0., c])
+
+    if bl=='eco':
+        b = 2*a
+        c = 3*a
+        basis = np.array([[0., 0., 0.],[a*0.5, b*0.5, 0.]])
+        a1 = np.array([a, 0., 0.])
+        a2 = np.array([0., b, 0.])
+        a3 = np.array([0., 0., c])
+
+    if bl=='fco':
+        b = 2*a
+        c = 3*a
+        basis = np.array([[0., 0., 0.],[a*0.5, b*0.5, 0.],[a*0.5, 0., c*0.5],[0., b*0.5, c*0.5]])
+        a1 = np.array([a, 0., 0.])
+        a2 = np.array([0., b, 0.])
+        a3 = np.array([0., 0., c])
+
 
     lattice = []
 
@@ -39,9 +92,9 @@ def main():
                 for x in range(n1):
                     R = x*a1 + y*a2 + z*a3 + basis[i]
 
-                    if R[0] <= n1-1:
-                        if R[1] <= n2-1:
-                            if R[2] <= n3-1:
+                    if R[0] <= (n1-1)*a:
+                        if R[1] <= (n2-1)*b:
+                            if R[2] <= (n3-1)*c:
                                 lattice.append(R)
 
     output = open(str(sys.argv[1]) + '.xyz', 'w')
