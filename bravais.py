e0c4aa740025e53d03d22b80924db26afadeada3
eselinger <elizabeth.selinger@uoit.net>
2015-01-17 20:30:29 -0500

    delete commented

--- bravais.py
+++ bravais.py
@@ -104,35 +104,6 @@ def main():
         a = lattice[j]
         output.write('X ' + ' ' + str(a[0]) + ' ' + str(a[1]) + ' ' + str(a[2]) + '\n')
 
-#going to do for loops in each direction using basis atoms
-#    for z in range(numcells): #place atoms in z
-#        for i in range(len(basis)):
-#            atom = basis[i] + a3*z
-#            lattice0.append(atom)
-#            lattice.append(atom)
-#    print lattice0
-
-#    lattice1 = []
-
-#    for y in range(numcells):
-#        for j in range(len(lattice0)):
-#            atom = lattice0[j] + a2*y
-#            lattice1.append(atom)
-#            lattice.append(atom)
-#    print lattice1
-
- #   for x in range(numcells): #repeat all atoms in x
- #       for k in range(len(lattice0) + len(lattice1)):
- #           atom = lattice[k] + a1*x
- #           lattice.append(atom)
-
-#    output = open('bravais_lattice.xyz', 'w')
-#    output.write(str(len(lattice)) + '\n \n')
-#   
-#    for a in range(len(lattice)):
-#        for element in lattice[a]: output.write(X + '     ' + str(element) + '     ')
-#        output.write('\n')
-
     output.close()
 
 main()
