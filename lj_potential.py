bdb3d42299340988911e56e6cae49f449460653b
eselinger <elizabeth.selinger@uoit.net>
2015-01-20 00:10:58 -0500

    calculating total energy and dividing by number of atoms

--- lj_potential.py
+++ lj_potential.py
@@ -1,14 +1,27 @@
-#/usr/bin/python
+#/usr/bin/pyython
 
 import sys
 import numpy as np
+import cdist from  scipy.spatial.distance
 
 def main():
+    infile = sys.argv[1]
+    readin = open(infile, 'r')
+
+    numat = int(readin.readline())
+    readin.readline()
+
+    lattice = np.array([readin.readlines()])    
+
     r_m=2
     eps=10
 
-    V = eps*((r_m/r)**12 - 2*(r_m/r)**6)
-    ETOT = np.sum(V)
-    E = ETOT/#of of atoms
+    for i in range(numat):
+        r = cdist(R,np.array(lattice[i])).flatten()
+        r = r[np.nonzero(r)]
+        V = eps*((r_m/r)**12 - 2*(r_m/r)**6)
+        ETOT += np.sum(V)
+    E = ETOT/(numat)
+    print E
 
 main()
